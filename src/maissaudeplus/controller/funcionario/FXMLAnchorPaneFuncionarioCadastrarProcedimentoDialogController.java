cc521ed83ae4060f9a98706f21c9b1954a095634
Raphael <raphael.pavani.1742@gmail.com>
2023-04-28 20:27:03 -0300

    CRUD do procedimento

--- src/maissaudeplus/controller/funcionario/FXMLAnchorPaneFuncionarioCadastrarProcedimentoDialogController.java
+++ src/maissaudeplus/controller/funcionario/FXMLAnchorPaneFuncionarioCadastrarProcedimentoDialogController.java
@@ -11,7 +11,10 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.CheckBox;
+import javafx.stage.Stage;
+import maissaudeplus.model.domain.Procedimento;
 
 /**
  * FXML Controller class
@@ -33,12 +36,98 @@ public class FXMLAnchorPaneFuncionarioCadastrarProcedimentoDialogController impl
     @FXML
     private JFXButton buttonCancelarCadastroProcedimento;
 
-    /**
-     * Initializes the controller class.
-     */
+    
+    
+    
+    //Atributos para a manipulação do Stage
+    private Stage dialogStage;
+    private boolean buttonConfirmarClicked = false;
+    private Procedimento procedimento;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+    }  
     
+    //Método de manipulação do botão "Confirmar"
+    @FXML
+    void handleButtonConfirmar() {
+        //If que só é executado caso todos os campos estejam no formato correto
+        if(validarEntradaDeDados()){
+            //Colocando os dados fornecidos na instância do paciente
+            procedimento.setNomeProcedimento(textFieldNomeProcedimento.getText().toUpperCase());
+            procedimento.setDescProcedimento(textFieldDescricaoProcedimento.getText().toUpperCase());
+            procedimento.setValorProcedimento(Double.parseDouble(textFieldValorProcedimento.getText()));
+            procedimento.setFlagObesidade(checkBoxProcedimento.isSelected());
+            //Retorna verdadeiro sinalizando que o botão "Confirmar" foi pressionado
+            buttonConfirmarClicked = true;
+            //Fechando o dialog
+            dialogStage.close();
+        }
+    }
+        
+    //Método de manipulação do botão "Cancelar"
+    @FXML
+    void handleButtonCancelar() {
+        //Fechando o dialog, note que não a necessidade de informar que este botão foi pressionado já que a flag "buttonConfirmarClicked" foi declarada como falsa
+        dialogStage.close();
+    }
+    
+    //Getter e Setters dos atributos de manipulação do stage
+    public Stage getDialogStage() {
+        return dialogStage;
+    }
+    
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+
+    public boolean isButtonConfirmarClicked() {
+        return buttonConfirmarClicked;
+    }
+
+    public Procedimento getProcedimento() {
+        return procedimento;
+    }
+
+    public void setProcedimento(Procedimento procedimento) {
+        this.procedimento = procedimento;
+    }
+    
+    //Método que valida os dados inseridos nos campos, note que cada campo necessitou de um tratamento diferente.
+    public boolean validarEntradaDeDados(){
+        //Mensagem de erro declarada como uma String vazia, serve de flag para o método
+        String errorMessage = "";
+        //If para válidar se o campo nome não esta vazio
+        if (textFieldNomeProcedimento.getText().length() == 0 || textFieldNomeProcedimento == null){
+            errorMessage += "Campo \"Nome\" inválido!\n";
+        }
+        if (textFieldDescricaoProcedimento.getText().length() == 0 || textFieldDescricaoProcedimento == null){
+            errorMessage += "Campo \"Nome\" inválido!\n";
+        }       
+        //If para válidar se o campo Peso não esta vazio e não contém letras
+        if (textFieldValorProcedimento.getText().length() == 0 || textFieldValorProcedimento == null){
+            errorMessage += "Campo \"Valor\" inválido!\n";
+        } else {
+            try{
+                Double.parseDouble(textFieldValorProcedimento.getText());
+            } catch (NumberFormatException e){
+            errorMessage += "Campo \"Valor\" inválido!\n";
+            }
+        }
+        //If para verificar se a String "errorMessage" esta vazia, caso ela esteja vazia, o metodo retorna true provando que os campos preenchidos estão corretos
+        if (errorMessage.length() == 0){
+            //Retorno verdadeiro sinalizando que os campos estão corretos
+            return true;
+        } else {
+            //Criação da mensagem de erro
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Erro no cadastro o procedimento");
+            alert.setHeaderText("Encontramos campos Inválidos, por favor verifique já.");
+            alert.setContentText(errorMessage);
+            alert.show();
+            //Retorno falso sinalizando que os campos estão errados
+            return false;
+        }
+    }
 }
--- src/maissaudeplus/model/dao/ProcedimentoDAO.java
+++ src/maissaudeplus/model/dao/ProcedimentoDAO.java
@@ -5,6 +5,126 @@
  */
 package maissaudeplus.model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import maissaudeplus.model.domain.Procedimento;
+
 public class ProcedimentoDAO {
     
+    
+    private Connection connection;
+    
+    public Connection getConnection(){
+        return connection;
+    }
+    
+    public void setConnection(Connection connection){
+        this.connection = connection;
+    }
+    
+    public List<Procedimento> listar(){
+        String sql = "SELECT * FROM procedimento";
+        List<Procedimento> retorno = new ArrayList();
+        try{
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet resultado = stmt.executeQuery();
+            while (resultado.next()){
+                Procedimento procedimento = new Procedimento();
+                procedimento.setCodProcedimento(resultado.getInt("codprocedimento"));
+                procedimento.setNomeProcedimento(resultado.getString("nomeprocedimento"));
+                procedimento.setDescProcedimento(resultado.getString("descprocedimento")); 
+                procedimento.setValorProcedimento(resultado.getDouble("valorprocedimento"));
+                procedimento.setFlagObesidade(resultado.getBoolean("flagobesidade"));
+                retorno.add(procedimento);
+            }
+        } catch(SQLException e){
+            Logger.getLogger(PacienteDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return retorno;
+    }
+    
+    public void inserir(Procedimento procedimento){
+        String sql = "INSERT INTO procedimento (nomeprocedimento, descprocedimento, valorprocedimento, flagobesidade) "
+                        + "VALUES (?, ?, ?, ?)";
+        try{
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, procedimento.getNomeProcedimento());
+            stmt.setString(2, procedimento.getDescProcedimento());
+            stmt.setDouble(3, procedimento.getValorProcedimento());
+            stmt.setBoolean(4, procedimento.isFlagObesidade());
+            stmt.execute();
+        } catch(SQLException e){
+            Logger.getLogger(PacienteDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+    
+    public boolean remover(Procedimento procedimento){
+        String sql = "DELETE FROM procedimento WHERE codprocedimento = ?;";
+        try{
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, procedimento.getCodProcedimento());
+            stmt.execute();
+            return true;
+        } catch (SQLException e){
+            Logger.getLogger(ProcedimentoDAO.class.getName()).log(Level.SEVERE, null, e);
+            return false;
+        }
+    }
+    
+    public boolean alterar(Procedimento procedimento){
+        String sql = "UPDATE procedimento SET nomeprocedimento=?, descprocedimento=?, valorprocedimento=?, flagobesidade=?";
+        try{
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, procedimento.getNomeProcedimento());
+            stmt.setString(2, procedimento.getDescProcedimento());
+            stmt.setDouble(3, procedimento.getValorProcedimento());
+            stmt.setBoolean(4, procedimento.isFlagObesidade());
+            stmt.execute();
+            return true;
+        } catch (SQLException e){
+            Logger.getLogger(PacienteDAO.class.getName()).log(Level.SEVERE, null, e);
+            return false;
+        }
+    }
+    
+    public Procedimento buscar(Procedimento paciente){
+        Procedimento retorno = new Procedimento();
+        String sql = "SELECT * FROM procedimento WHERE codprocedimento=?";
+        try{
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, paciente.getCodProcedimento());
+            ResultSet resultado = stmt.executeQuery();
+            if (resultado.next()){
+                retorno.setCodProcedimento(resultado.getInt("codprocedimento"));
+                retorno.setNomeProcedimento(resultado.getString("nomeprocedimento"));
+                retorno.setDescProcedimento(resultado.getString("descprocedimento")); 
+                retorno.setValorProcedimento(resultado.getDouble("valorprocedimento"));
+                retorno.setFlagObesidade(resultado.getBoolean("flagobesidade"));;
+            }
+        } catch(SQLException e) {
+            Logger.getLogger(PacienteDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return retorno;
+    }
+        public String getNomeProcedimento(Procedimento procedimento){
+        String sql = "SELECT nomeprocedimento FROM procedimento WHERE codprocedimento = ?;";
+        String retorno = "";
+        try{
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, procedimento.getCodProcedimento());
+            ResultSet resultado = stmt.executeQuery();
+            while(resultado.next()){
+                retorno = resultado.getString("nomeprocedimento");
+            }
+        } catch (SQLException e){
+            Logger.getLogger(PacienteDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return retorno;
+    }
 }
